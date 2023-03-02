3f0f563c43987d8ae76464ce066c5b9d01bded1c
CyberTapir <callumtbennett@gmail.com>
2023-03-03 09:44:07 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        btnRead = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(61, 51)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(196, 334)
+        ListBox1.TabIndex = 0
+        ' 
+        ' btnRead
+        ' 
+        btnRead.Location = New Point(492, 156)
+        btnRead.Name = "btnRead"
+        btnRead.Size = New Size(75, 23)
+        btnRead.TabIndex = 1
+        btnRead.Text = "Read"
+        btnRead.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnRead)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnRead As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        FileSystem.FileOpen(1, "\\Mac\Home\Desktop\numbers.txt.txt", OpenMode.Input)
+
+    End Sub
+End Class
